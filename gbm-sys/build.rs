cfecc4a8cf98607467c4197f1cfbd3374d4d4d67
K <kayo@illumium.org>
2021-03-25 09:16:31 +0500

    Added helper for updating pre-built bindings

--- gbm-sys/build.rs
+++ gbm-sys/build.rs
@@ -1,16 +1,14 @@
 #[cfg(feature = "gen")]
 extern crate bindgen;
 
-#[cfg(feature = "gen")]
-use std::env;
-#[cfg(feature = "gen")]
-use std::path::Path;
-
 #[cfg(not(feature = "gen"))]
 fn main() {}
 
 #[cfg(feature = "gen")]
 fn main() {
+    use std::env;
+    use std::path::Path;
+
     // Setup bindings builder
     let generated = bindgen::builder()
         .header("include/gbm.h")
@@ -30,4 +28,28 @@ fn main() {
     let dest_path = Path::new(&out_dir).join(bind_name);
 
     generated.write_to_file(dest_path).unwrap();
+
+    #[cfg(feature = "update_bindings")]
+    {
+        use std::{fs, io::Write};
+
+        let bind_file = Path::new(&out_dir).join(bind_name);
+        let dest_dir = Path::new("src")
+            .join("platforms")
+            .join(env::var("CARGO_CFG_TARGET_OS").unwrap())
+            .join(env::var("CARGO_CFG_TARGET_ARCH").unwrap());
+        let dest_file = dest_dir.join(bind_name);
+
+        fs::create_dir_all(&dest_dir).unwrap();
+        fs::copy(&bind_file, &dest_file).unwrap();
+
+        if let Ok(github_env) = env::var("GITHUB_ENV") {
+            let mut env_file = fs::OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(github_env)
+                .unwrap();
+            write!(env_file, "GBM_SYS_BINDINGS_FILE={}", dest_file.display()).unwrap();
+        }
+    }
 }
